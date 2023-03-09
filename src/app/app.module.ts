a57eee0d65ea0b451ca4fb8157d6310805edaffd
Ramon Faundez <ramon.faundez@haulmer.com>
2023-03-08 22:13:53 -0300

    [ADD]views necessary to work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ErrorComponent } from './error/error.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { AccessComponent } from './access/access.component';
+import { PermissionsComponent } from './permissions/permissions.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorComponent,
+    HomeComponent,
+    UsersComponent,
+    AccessComponent,
+    PermissionsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
