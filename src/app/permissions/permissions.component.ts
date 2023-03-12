9b763a23b9383bd1ae7056d192593103d55e0a5d
Ramon Faundez <ramon.faundez@haulmer.com>
2023-03-12 04:11:41 -0300

    [ADD]list of permissions

--- src/app/permissions/permissions.component.ts
+++ src/app/permissions/permissions.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { IPermission } from '../../assets/interfaces/interfaces';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-permissions',
   templateUrl: './permissions.component.html',
   styleUrls: ['./permissions.component.scss']
 })
-export class PermissionsComponent {
+export class PermissionsComponent implements OnInit {
+
+  public roleList!: Array<IPermission>;
+  public displayedColumns: Array<string> = ['role','usersTable', 'permissionsTable', 'logsTable']
+  public dataSource!: any;
+
+  constructor(
+    private apiService: ApiService
+  ) { }
+
+  ngOnInit(): void {
+    this.getAllPermissions()
+  }
+
+  private getAllPermissions(): void {
+    this.apiService.getAllPermissions().subscribe(res => {
+      this.dataSource = new MatTableDataSource<IPermission>(res.data);
+    })
+  }
 
 }
